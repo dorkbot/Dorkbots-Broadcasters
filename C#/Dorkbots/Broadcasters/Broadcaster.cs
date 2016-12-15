6216a19ef542238bb0358f4dcae95c218b772496
dorkbot <dayvid@dayvid.com>
2016-12-15 12:03:28 -0800

    Added C# version

---
+++ C#/Dorkbots/Broadcasters/Broadcaster.cs
@@ -0,0 +1,60 @@
+﻿/*
+* Author: Dayvid jones
+* http://www.dayvid.com
+* Copyright (c) Superhero Robot 2016
+* http://www.superherorobot.com
+* Version: 1.0.0
+* 
+* Licence Agreement
+*
+* You may distribute and modify this class freely, provided that you leave this header intact,
+* and add appropriate headers to indicate your changes. Credit is appreciated in applications
+* that use this code, but is not required.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+
+using System;
+
+namespace Dorkbots.Broadcasters
+{
+    public class Broadcaster : IBroadcaster
+    {
+        public Broadcaster()
+        {
+        }
+
+        public void SendBroadcast(string message, object sender, IBroadcaster broadcaster, object dataObject = null)
+        {
+            OnBroadcastEvent(new BroadcasterEvent(message, sender, broadcaster, dataObject));
+        }
+
+        private event EventHandler _BroadcastEvent;
+        public EventHandler BroadcastEvent{ get{ return _BroadcastEvent; } set{ _BroadcastEvent = value; } }
+        private void OnBroadcastEvent(BroadcasterEvent e)
+        {
+            EventHandler handler = _BroadcastEvent;
+            if (handler != null)
+            {
+                handler(this, e);
+            }
+        }
+    }
+}
---
+++ C#/Dorkbots/Broadcasters/BroadcasterEvent.cs
@@ -0,0 +1,57 @@
+﻿/*
+* Author: Dayvid jones
+* http://www.dayvid.com
+* Copyright (c) Superhero Robot 2016
+* http://www.superherorobot.com
+* Version: 1.0.0
+* 
+* Licence Agreement
+*
+* You may distribute and modify this class freely, provided that you leave this header intact,
+* and add appropriate headers to indicate your changes. Credit is appreciated in applications
+* that use this code, but is not required.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*/
+
+
+using System;
+
+namespace Dorkbots.Broadcasters
+{
+    public class BroadcasterEvent : EventArgs
+    {
+        private string _message;
+        public string message{ get{ return _message; } }
+        private object _sender;
+        public object sender{ get{ return _sender; } }
+        private IBroadcaster _broadcaster;
+        public IBroadcaster broadcaster{ get{ return _broadcaster; } }
+        private object _dataObject;
+        public object dataObject{ get{ return _dataObject; } }
+
+        public BroadcasterEvent(string message, object sender, IBroadcaster broadcaster, object dataObject = null)
+        {
+            _message = message;
+            _sender = sender;
+            _broadcaster = broadcaster;
+            _dataObject = dataObject;
+        }
+    }
+}
